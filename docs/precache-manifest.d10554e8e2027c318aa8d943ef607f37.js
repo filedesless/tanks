3cb963a59b3c07fcca990edad4806da9c2b206d0
filedesless <filedesless@gmail.com>
2020-04-23 01:24:37 -0400

    build into dosc/

---
+++ docs/precache-manifest.d10554e8e2027c318aa8d943ef607f37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "489688679a60f484f82a591f96331ff2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14b63020b6e2dd9aeac0",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "2cd78f09052c7afd7c62",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "14b63020b6e2dd9aeac0",
+    "url": "/static/js/2.323b15be.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.323b15be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cd78f09052c7afd7c62",
+    "url": "/static/js/main.51c7d08d.chunk.js"
+  },
+  {
+    "revision": "37bdc833eaacd7d55c98",
+    "url": "/static/js/runtime-main.e7422d39.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
